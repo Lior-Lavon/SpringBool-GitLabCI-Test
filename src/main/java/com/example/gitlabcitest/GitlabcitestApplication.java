e41bd707cbf7184dcf304e29b5d726838f013e40
Lior Lavon <>
2020-05-07 00:07:56 +0200

    first push to GitHub

---
+++ src/main/java/com/example/gitlabcitest/GitlabcitestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlabcitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabcitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabcitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitlabcitest/GitlabcitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlabcitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabcitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
